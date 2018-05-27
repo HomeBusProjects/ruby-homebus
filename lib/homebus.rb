7ab9a0acb8a9a145022ebc0077c452b0cac4b410
John Romkey <romkey@romkey.com>
2018-05-26 21:09:17 -0700

    updated for new provisioner

--- lib/homebus.rb
+++ lib/homebus.rb
@@ -8,24 +8,35 @@ class HomeBus
   # you get back another JSON object
   # uuid, mqtt_hostname, mqtt_port, mqtt_username, mqtt_password
   # save this in .env.provision and return it in the mqtt parameter
-  def self.provision(mac_address)
+  def self.provision(args)
     uri = URI('http://127.0.0.1:3000/provision')
 
-    request = {
-      provision: {
-        mac_address: mac_address
-      }
-    }
+#    provision = {}
+#    provision.merge args
+#    provision.require([:friendly_name, :manufacturer, :model_number, :serial_number, :pin, :wo_topics, :ro_topics, :rw_topics])
+
+    provision = args
 
     req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
-    req.body = request.to_json
+    req.body = {
+      provision: provision
+    }.to_json
 
-    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-      res = http.request(req)
 
-      return nil unless res.code == "200"
+    status = "waiting"
 
-      answer = JSON.parse res.body, symbolize_names: true
+    # make sure answer is in scope
+    answer = nil
+    begin
+      res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+        res = http.request(req)
+
+        return nil unless res.code == "200" || res.code == '201'
+
+        answer = JSON.parse res.body, symbolize_names: true
+        status = answer[:status]
+        sleep(answer[:retry_time].to_i) if(status == 'waiting')
+      end while(status == 'waiting')
 
       mqtt = Hash.new
       mqtt[:host] = answer[:mqtt_hostname]
